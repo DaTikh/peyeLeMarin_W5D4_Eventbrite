0ebc409ab2c257d44fd90a1c39884e1d9e007064
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-05-03 12:24:56 +0200

    "views Users OK" manque signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(params_user)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:error] = "Couldn't save."
+      render action: "new"
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def params_user
+    params.require(:user).permit(:name)
   end
 end
