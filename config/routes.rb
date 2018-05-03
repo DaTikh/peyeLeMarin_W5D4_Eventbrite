79c897ad07f40301a2c7ebe0bfd45dffd19fcd98
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-05-03 12:04:11 +0200

    création users_controller, header

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "users#new"
+  get "/user/new", to: "users#new", as: "new_user"
+  post "/user/:id", to: 'users#create', as: "create_user"
+  get "/user/:id", to: 'users#show', as: "user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
